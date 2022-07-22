eff05f139dc7a3ff8ced3a2496d5b09dfc58c3bb
dpzzpatel <dpzzpatel@gmail.com>
2022-07-22 21:49:35 +0530

    maxprofit problem solution

---
+++ index.js
@@ -0,0 +1,47 @@
+const { stdin, stdout } = process;
+
+function prompt(question) {
+  return new Promise((resolve, reject) => {
+    stdin.resume();
+    stdout.write(question);
+    stdin.on('data', data => resolve(data.toString().trim()));
+    stdin.on('error', err => reject(err));
+  });
+}
+var time = 0;
+var timearr = [5,4,10];
+var earningspertime = [1500,1000,3000];
+var solutions = [];
+var totalearnings = 0;
+
+function calculatemaxearnings (time,earnings,possiblesolution){
+    if(earnings > totalearnings){
+        totalearnings = earnings;
+        solutions = possiblesolution.map((ele)=>{return ele});
+    }
+    else if(earnings == totalearnings){
+        solutions.push(possiblesolution.map((ele)=>{
+            return (ele);
+        }));
+    }
+    for(var i=0; i<3;i++){
+        if(timearr[i] < time){
+            possiblesolution[i] += 1;
+            calculatemaxearnings(time - timearr[i],((time-timearr[i])*earningspertime[i]) + earnings,possiblesolution);
+            possiblesolution[i] -= 1;
+        }
+    }
+}
+const main = async()=>{
+        time = parseInt(await prompt("Enter Time: "));
+        calculatemaxearnings(time,0,new Array(3).fill(0));   
+        console.log("Earnings: $"+totalearnings);
+        console.log(solutions);
+        solutions.forEach((solution,index)=>{
+            console.log(solution);
+            console.log(index+1+". T: "+solution[0]+" P: "+solution[1]+" C: "+solution[2]);
+        })
+    }
+
+main();
+
